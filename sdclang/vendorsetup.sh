90783b974f1a0ba2836597c64c2a1a34e7797ce4
Miguel Martinez <slverwolf@gmail.com>
2018-10-23 16:19:27 -0500

    sdclang: Switch to dragontc-7

--- sdclang/vendorsetup.sh
+++ sdclang/vendorsetup.sh
@@ -1,6 +1,6 @@
 # Set SDClang defaults
 export SDCLANG=false
-export SDCLANG_PATH=vendor/qcom/dragontc-6.0/bin/
+export SDCLANG_PATH=vendor/qcom/dragontc-7.0/bin/
 export SDCLANG_LTO_DEFS=vendor/skydragon/sdclang/sdllvm-lto-defs.mk
 export SDCLANG_COMMON_FLAGS="-Ofast -g0 -mcpu=cortex-a57 -Wno-user-defined-warnings -Wno-vectorizer-no-neon -Wno-unknown-warning-option \
 -Wno-deprecated-register -Wno-tautological-type-limit-compare -Wno-sign-compare -Wno-gnu-folding-constant \
