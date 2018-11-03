d1e94acf13a4c6540fa408d22cca9e58b3148159
HolyAngel <slverwolf@gmail.com>
2018-11-03 10:16:52 -0500

    Update sepolicy call

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,7 +1,9 @@
 #
 # This policy configuration will be used by all products that
-# inherit from pure
+# inherit from SkyDragon
 #
 
 BOARD_SEPOLICY_DIRS += \
     vendor/skydragon/sepolicy
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/skydragon/sepolicy/private
